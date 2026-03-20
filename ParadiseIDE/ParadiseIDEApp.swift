4894512ad85c2ea6d6ab02f12f561e442e51c70f
Chayce22315 <chayceray@outlook.com>
2026-03-20 22:57:55 +0000

    added missing ParadiseIDEApp.swift file

---
+++ ParadiseIDE/ParadiseIDEApp.swift
@@ -0,0 +1,19 @@
+import SwiftUI
+
+@main
+struct ParadiseIDEApp: App {
+@StateObject private var editorVM = EditorViewModel()
+@StateObject private var folderManager = FolderManager()
+
+```
+var body: some Scene {
+    WindowGroup {
+        ContentView()
+            .environmentObject(editorVM)
+            .environmentObject(folderManager)
+            .preferredColorScheme(.dark)
+    }
+}
+```
+
+}
