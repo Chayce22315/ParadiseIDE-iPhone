06d0fe50e14f320b62dde37b3b835c58ec641e61
Chayce22315 <chayceray@outlook.com>
2026-03-20 23:06:26 +0000

    fixed backticks

--- ParadiseIDE/ParadiseIDEApp.swift
+++ ParadiseIDE/ParadiseIDEApp.swift
@@ -2,18 +2,15 @@ import SwiftUI
 
 @main
 struct ParadiseIDEApp: App {
-@StateObject private var editorVM = EditorViewModel()
-@StateObject private var folderManager = FolderManager()
+    @StateObject private var editorVM = EditorViewModel()
+    @StateObject private var folderManager = FolderManager()
 
-```
-var body: some Scene {
-    WindowGroup {
-        ContentView()
-            .environmentObject(editorVM)
-            .environmentObject(folderManager)
-            .preferredColorScheme(.dark)
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environmentObject(editorVM)
+                .environmentObject(folderManager)
+                .preferredColorScheme(.dark)
+        }
     }
-}
-```
-
 }
